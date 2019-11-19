7ffed83f3c3a8da7de00d21f22638bbd39abe69f
jtas <jtas@uw.edu>
2019-11-19 15:41:19 -0800

    trying out how to print strings and take in command line input. Need to test out variable setting to see how to define / use ints, strings, classes, etc.

--- PythonTesting.py
+++ PythonTesting.py
@@ -1,5 +1,47 @@
+# use pound signs to define a comment for a line
+""" This is how comments
+can go across multiple
+lines so you don't have
+to keep putting hastags
+every line of an essay"""
+
 print("Hello world!\n")
 print("Who are you?")
-firstVariable = input()
-print("Was " + firstVariable + " your input?")
-print("Whatever...\n")
+first_variable = input()
+print("Was " + first_variable + " your input?")
+print("Whatever...")
+
+''' Indentation in Python is the 
+equivalent of using {} in Java. No
+need to use semicolons to terminate
+every command'''
+
+# for loop example (credit: google search)
+my_string = "python"
+x = 0
+for i in my_string:
+    x = x + 1
+    print(my_string[0:x])
+
+for i in my_string:
+    x = x - 1
+    print(my_string[0:x])
+
+for i in range(0,len(my_string)):
+    print(my_string[i] + " "),
+
+''' output:
+p
+py
+pyt
+pyth
+pytho
+python
+pytho
+pyth
+pyt
+py
+p
+'''
+
+print("\n")
